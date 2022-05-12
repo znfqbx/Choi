d0765b1672eb8310dc768634a396a9344630c6f8
chou_chou <znfqbx@naver.com>
2022-05-12 22:15:57 +0900

    esm

--- hello-react/blog/blog-backend/src/index.js
+++ hello-react/blog/blog-backend/src/index.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
+
 const { faDiagramNext } = require('@fortawesome/free-solid-svg-icons');
 const Koa = require('koa');
 const { useCallback } = require('react');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+//비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URI } = process.env;
 
 const api = require('./api');
 
@@ -32,6 +38,17 @@ app.use(bodyParser());
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+//PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
