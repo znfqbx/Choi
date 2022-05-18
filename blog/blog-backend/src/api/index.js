9bf1f14c307f1c7b3d7d96e97ca12c88c922dba8
chou_chou <znfqbx@naver.com>
2022-05-18 23:59:15 +0900

    register,login

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 import posts from './posts';
+import auth from './auth';
 
 const api = new Router();
 
 api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
 // api.get('/test', (ctx) => {
 //   ctx.body = 'test 성공';
