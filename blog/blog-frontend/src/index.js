8d09696a25e035373adefda44860b26057cc5da2
chou_chou <znfqbx@naver.com>
2022-05-23 01:10:40 +0900

    Redux

--- blog/blog-frontend/src/index.js
+++ blog/blog-frontend/src/index.js
@@ -9,12 +9,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from '../node_modules/react-redux/es/exports';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules/index';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </Provider>,
   // document.getElementById('root'),
 );
