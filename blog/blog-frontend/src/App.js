6cf6c0742b0ac6a2c59901c9f3fd53e4b2ce3ce6
chou_chou <znfqbx@naver.com>
2022-05-30 16:46:37 +0900

    5.30(1)

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import { Route, Routes } from '../node_modules/react-router-dom/index';
-import PostListPage from './pages/PostListPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import WritePage from './pages/WritePage';
-import PostPage from './pages/PostPage';
-// import { Routes } from '../node_modules/react-router-dom/index';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PostListPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/write" element={<WritePage />} />
-      <Route path="/@:username">
-        <Route index element={<PostListPage />} />
-        <Route path=":postId" element={<PostPage />} />
-      </Route>
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
+
 export default App;
--- blog/blog-frontend/src/index.js
+++ blog/blog-frontend/src/index.js
@@ -1,40 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import { BrowserRouter } from '../node_modules/react-router-dom/index';
-// import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from '../node_modules/react-redux/es/exports';
-import { createStore } from 'redux';
-import { applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga';
-import rootReducer from './modules/index';
-import { rootSaga } from './modules/index';
-
-// const store = createStore(rootReducer, composeWithDevTools());
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware))
-);
-
-sagaMiddleware.run(rootSaga);
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-      여기는 src index.js 이고 <br></br>
-      몬구 바뷰바뷰
-    </BrowserRouter>
-    
-  </Provider>,
-  // document.getElementById('root'),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
