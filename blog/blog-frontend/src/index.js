f9ca1da7785c12968830dbf300a45fcdc3f6deae
chou_chou <znfqbx@naver.com>
2022-05-31 14:06:21 +0900

    Register

--- blog/blog-frontend/src/index.js
+++ blog/blog-frontend/src/index.js
@@ -6,10 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from './modules/index';
 import rootReducer from './modules/index';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -1,8 +1,16 @@
 import { combineReducers } from "redux";
+import { all } from 'redux-saga/effects';
 import auth from "./auth";
+import { authSaga } from "./auth";
+import loading from "./loading";
+import user from "./user";
+import { userSaga } from "./user";
 
 const rootReducer = combineReducers({
-    auth,
+    auth,loading,user,
   });
   
+  export function* rootSaga() {
+    yield all([authSaga(), userSaga()]);
+  }
   export default rootReducer;
